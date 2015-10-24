48d719a69a020d4298a93dffbf49f30c22f2848d
Abelatox <abelatox@hotmail.com>
2015-10-24 14:51:06 +0200

    gf

--- src/main/java/wehavecookies56/kk/network/packet/server/magics/MagicFire.java
+++ src/main/java/wehavecookies56/kk/network/packet/server/magics/MagicFire.java
@@ -33,7 +33,6 @@ public class MagicFire extends AbstractServerMessage<MagicFire> {
 		{
 			ExtendedPlayer.get(player).removeMp(Constants.FIRE_COST);
 		}
-		
 		World world = player.worldObj;
 		switch(ExtendedPlayer.get(player).getMagicLevel("Fire")){
 		case 1:
