d59e2122e0f8d71990295630730e1333e9b630f0
Abelatox <abelatox@hotmail.com>
2015-10-25 00:00:58 +0200

    Command things for multiplayer

--- src/main/java/wehavecookies56/kk/server/command/CommandRemoveKeychain.java
+++ src/main/java/wehavecookies56/kk/server/command/CommandRemoveKeychain.java
@@ -2,14 +2,19 @@ package wehavecookies56.kk.server.command;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.PlayerNotFoundException;
+import net.minecraft.command.PlayerSelector;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
 import wehavecookies56.kk.entities.ExtendedPlayer;
@@ -44,7 +49,7 @@ public class CommandRemoveKeychain implements ICommand {
 	
 	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		return "/removechain <player>";
+		return "/removechain [player]";
 	}
 
 	@Override
@@ -68,15 +73,11 @@ public class CommandRemoveKeychain implements ICommand {
 		if(sender.getCommandSenderEntity() instanceof EntityPlayer){
 			ExtendedPlayer props = ExtendedPlayer.get((EntityPlayer) sender.getCommandSenderEntity());
 			EntityPlayer player = (EntityPlayer) sender.getCommandSenderEntity();
-			if(args.length != 1)
-			{
-				TextHelper.sendFormattedChatMessage("Invalid arguments! Usage: /removechain <player>", EnumChatFormatting.RED, (EntityPlayer) sender.getCommandSenderEntity());
-			}
-			else
+			if(args.length == 0)
 			{
 				if(props.inventory.getStackInSlot(0) != null)
 				{
-										props.inventory.getStackInSlot(0).setItem(Items.paper);
+					props.inventory.getStackInSlot(0).setItem(Items.paper);
 					if(props.isKeybladeSummoned())
 					{
 						if(player.inventory.getCurrentItem() != null && player.inventory.getCurrentItem().getItem() instanceof ItemKeyblade){// && props.isKeybladeSummoned() == true
@@ -84,12 +85,38 @@ public class CommandRemoveKeychain implements ICommand {
 						}
 					}
 					PacketDispatcher.sendToServer(new SyncExtendedPlayer(player));
+					TextHelper.sendFormattedChatMessage("Your keychain has been removed!", EnumChatFormatting.YELLOW, (EntityPlayer) sender.getCommandSenderEntity());
+				}
+				else
+				{
+					TextHelper.sendFormattedChatMessage("The chain slot has no chain!", EnumChatFormatting.RED, (EntityPlayer) sender.getCommandSenderEntity());
+				}
+			}
+			else if(args.length == 1)
+			{
+	            EntityPlayerMP playermp = args.length == 1 ? getPlayer(sender, args[0]) : getCommandSenderAsPlayer(sender);
+				ExtendedPlayer propsmp = ExtendedPlayer.get(playermp);
+
+				if(propsmp.inventory.getStackInSlot(0) != null)
+				{
+					propsmp.inventory.getStackInSlot(0).setItem(Items.paper);
+					if(propsmp.isKeybladeSummoned())
+					{
+						if(playermp.inventory.getCurrentItem() != null && playermp.inventory.getCurrentItem().getItem() instanceof ItemKeyblade){// && propsmp.isKeybladeSummoned() == true
+							PacketDispatcher.sendToServer(new DeSummonKeyblade(playermp.inventory.getCurrentItem()));
+						}
+					}
+					PacketDispatcher.sendToServer(new SyncExtendedPlayer(playermp));
 					TextHelper.sendFormattedChatMessage(args[0]+"'s keychain has been removed!", EnumChatFormatting.YELLOW, (EntityPlayer) sender.getCommandSenderEntity());
 				}
 				else
 				{
 					TextHelper.sendFormattedChatMessage("The chain slot has no chain!", EnumChatFormatting.RED, (EntityPlayer) sender.getCommandSenderEntity());
 				}
+			}
+			else
+			{
+				TextHelper.sendFormattedChatMessage("Invalid arguments! Usage: /removechain [player]", EnumChatFormatting.RED, (EntityPlayer) sender.getCommandSenderEntity());
 			}
 		}
 	}
@@ -107,6 +134,49 @@ public class CommandRemoveKeychain implements ICommand {
 
 	@Override
 	public boolean isUsernameIndex(String[] args, int index) {
-		return false;
+		return true;
 	}
+	
+	public static EntityPlayerMP getCommandSenderAsPlayer(ICommandSender sender) throws PlayerNotFoundException
+	{
+	    if (sender instanceof EntityPlayerMP)
+	    {
+	        return (EntityPlayerMP)sender;
+	    }
+	    else
+	    {
+	        throw new PlayerNotFoundException("You must specify which player you wish to perform this action on.", new Object[0]);
+	    }
+	}
+	
+	public static EntityPlayerMP getPlayer(ICommandSender sender, String username) throws PlayerNotFoundException
+    {
+        EntityPlayerMP entityplayermp = PlayerSelector.matchOnePlayer(sender, username);
+
+        if (entityplayermp == null)
+        {
+            try
+            {
+                entityplayermp = MinecraftServer.getServer().getConfigurationManager().getPlayerByUUID(UUID.fromString(username));
+            }
+            catch (IllegalArgumentException illegalargumentexception)
+            {
+                ;
+            }
+        }
+
+        if (entityplayermp == null)
+        {
+            entityplayermp = MinecraftServer.getServer().getConfigurationManager().getPlayerByUsername(username);
+        }
+
+        if (entityplayermp == null)
+        {
+            throw new PlayerNotFoundException();
+        }
+        else
+        {
+            return entityplayermp;
+        }
+    }
 }
--- src/main/java/wehavecookies56/kk/server/command/CommandResetLevel.java
+++ src/main/java/wehavecookies56/kk/server/command/CommandResetLevel.java
@@ -2,11 +2,16 @@ package wehavecookies56.kk.server.command;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.PlayerNotFoundException;
+import net.minecraft.command.PlayerSelector;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
 import wehavecookies56.kk.entities.ExtendedPlayer;
@@ -41,7 +46,7 @@ public class CommandResetLevel implements ICommand {
 	
 	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		return "/resetlevel";
+		return "/resetlevel [playername]";
 	}
 
 	@Override
@@ -75,9 +80,22 @@ public class CommandResetLevel implements ICommand {
 				TextHelper.sendFormattedChatMessage("You level has been reset", EnumChatFormatting.YELLOW, (EntityPlayer) sender.getCommandSenderEntity());
 
 			}
+			else if(args.length == 1)
+			{
+	            EntityPlayerMP entityplayermp = args.length == 1 ? getPlayer(sender, args[0]) : getCommandSenderAsPlayer(sender);
+	            ExtendedPlayer.get(entityplayermp).level = 1;
+				ExtendedPlayer.get(entityplayermp).experience = 0;
+				ExtendedPlayer.get(entityplayermp).setStrength(1);
+				ExtendedPlayer.get(entityplayermp).setDefense(1);
+				ExtendedPlayer.get(entityplayermp).setMagic(1);
+				ExtendedPlayer.get(entityplayermp).setHP(20);
+				player.heal(ExtendedPlayer.get(entityplayermp).getHP());
+				TextHelper.sendFormattedChatMessage(args[0]+"'s level has been reset", EnumChatFormatting.YELLOW, (EntityPlayer) sender.getCommandSenderEntity());
+
+			}
 			else
 			{
-				TextHelper.sendFormattedChatMessage("Invalid arguments, usage: /resetlevel", EnumChatFormatting.RED, (EntityPlayer) sender.getCommandSenderEntity());
+				TextHelper.sendFormattedChatMessage("Invalid arguments, usage: /resetlevel [player]", EnumChatFormatting.RED, (EntityPlayer) sender.getCommandSenderEntity());
 			}
 		}
 	}
@@ -97,4 +115,46 @@ public class CommandResetLevel implements ICommand {
 	public boolean isUsernameIndex(String[] args, int index) {
 		return false;
 	}
+	
+	public static EntityPlayerMP getCommandSenderAsPlayer(ICommandSender sender) throws PlayerNotFoundException
+	{
+	    if (sender instanceof EntityPlayerMP)
+	    {
+	        return (EntityPlayerMP)sender;
+	    }
+	    else
+	    {
+	        throw new PlayerNotFoundException("You must specify which player you wish to perform this action on.", new Object[0]);
+	    }
+	}
+	public static EntityPlayerMP getPlayer(ICommandSender sender, String username) throws PlayerNotFoundException
+    {
+        EntityPlayerMP entityplayermp = PlayerSelector.matchOnePlayer(sender, username);
+
+        if (entityplayermp == null)
+        {
+            try
+            {
+                entityplayermp = MinecraftServer.getServer().getConfigurationManager().getPlayerByUUID(UUID.fromString(username));
+            }
+            catch (IllegalArgumentException illegalargumentexception)
+            {
+                ;
+            }
+        }
+
+        if (entityplayermp == null)
+        {
+            entityplayermp = MinecraftServer.getServer().getConfigurationManager().getPlayerByUsername(username);
+        }
+
+        if (entityplayermp == null)
+        {
+            throw new PlayerNotFoundException();
+        }
+        else
+        {
+            return entityplayermp;
+        }
+    }
 }
