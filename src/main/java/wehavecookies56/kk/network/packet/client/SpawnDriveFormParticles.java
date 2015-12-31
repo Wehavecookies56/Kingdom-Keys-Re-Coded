760635fc4265d087efc26b73e823779800b07bdb
Abelatox <abel54225@gmail.com>
2015-12-31 20:01:30 +0100

    Fixed the particle spawning when driving in a form without license

--- src/main/java/wehavecookies56/kk/network/packet/client/SpawnDriveFormParticles.java
+++ src/main/java/wehavecookies56/kk/network/packet/client/SpawnDriveFormParticles.java
@@ -41,14 +41,36 @@ public class SpawnDriveFormParticles extends AbstractClientMessage<SpawnDriveFor
 	@Override
 	public void process(EntityPlayer player, Side side) {
 		double r;
-		r = 1.5D;
+		r = 1.2D;
 		for(int a = 1; a <= 360; a+=7){
-			double x = this.x + (r * Math.cos(Math.toRadians(a)));
-			double z = this.z + (r * Math.sin(Math.toRadians(a)));
-			System.out.println("kidhfdeijofiefjoisweh");
-			player.worldObj.spawnParticle(EnumParticleTypes.FLAME, x, this.y + 1.25D, z, 0.0D, 0.0D, 0.0D);
-			player.worldObj.spawnParticle(EnumParticleTypes.FLAME, x, this.y + 1.05D, z, 0.0D, 0.0D, 0.0D);
-		
+			for(double i = 0; i<2.5; i= i+0.2)
+			{
+				double x = player.posX + (r * Math.cos(Math.toRadians(a)));
+				double z = player.posZ + (r * Math.sin(Math.toRadians(a)));
+				if(i < 0.5 || i > 2)
+				{
+					x = player.posX + (r/1.3 * Math.cos(Math.toRadians(a)));
+					z = player.posZ + (r/1.3 * Math.sin(Math.toRadians(a)));
+				}
+				if(i < 0.3 || i > 2.2)
+				{
+					x = player.posX + (r/1.5 * Math.cos(Math.toRadians(a)));
+					z = player.posZ + (r/1.5 * Math.sin(Math.toRadians(a)));
+				}
+				if(i > 1 && i < 1.5)
+				{
+					x = player.posX + (r*1.1 * Math.cos(Math.toRadians(a)));
+					z = player.posZ + (r*1.1 * Math.sin(Math.toRadians(a)));
+				}
+				/*double x2 = player.posX + (r/1.3 * Math.cos(Math.toRadians(a)));
+				double z2 = player.posZ + (r/1.3 * Math.sin(Math.toRadians(a)));*/
+				player.worldObj.spawnParticle(EnumParticleTypes.CRIT_MAGIC, x, player.posY + i, z, 0.0D, 0.3, 0.0D);
+		/*	player.worldObj.spawnParticle(EnumParticleTypes.CRIT_MAGIC, x, player.posY + 2.0D, z, 0.0D, 0.0D, 0.0D);
+			player.worldObj.spawnParticle(EnumParticleTypes.CRIT_MAGIC, x, player.posY + 1.5D, z, 0.0D, 0.0D, 0.0D);
+			player.worldObj.spawnParticle(EnumParticleTypes.CRIT_MAGIC, x, player.posY + 1.0D, z, 0.0D, 0.0D, 0.0D);
+			player.worldObj.spawnParticle(EnumParticleTypes.CRIT_MAGIC, x, player.posY + 0.5D, z, 0.0D, 0.0D, 0.0D);
+			player.worldObj.spawnParticle(EnumParticleTypes.CRIT_MAGIC, x2, player.posY + 0.0D, z2, 0.0D, 0.0D, 0.0D);*/
+			}
 		}
 	}
 }
