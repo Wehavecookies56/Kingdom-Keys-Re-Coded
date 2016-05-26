f767f7d59f4721cdfe9e08d2b85f3cd4cf599c08
Abelatox <abel54225@gmail.com>
2016-05-26 16:31:26 +0200

    Some keyblades

--- src/main/java/uk/co/wehavecookies56/kk/common/item/base/ItemRealKeyblade.java
+++ src/main/java/uk/co/wehavecookies56/kk/common/item/base/ItemRealKeyblade.java
@@ -1,16 +1,11 @@
 package uk.co.wehavecookies56.kk.common.item.base;
 
-import java.util.List;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ItemRealKeyblade extends ItemKeyblade {
 
-	public ItemRealKeyblade (ToolMaterial material) {
-		super(material);
+	public ItemRealKeyblade (int strength, int magic) {
+		super(strength, magic);
 		setMaxStackSize(1);
 	}
 }
