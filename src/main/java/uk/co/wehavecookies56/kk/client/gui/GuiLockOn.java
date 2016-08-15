68f1f69437be76ae30d4ebf811e492d7b5cb844c
Abelatox <abel54225@gmail.com>
2016-08-15 16:21:25 +0200

    Started Scan

--- src/main/java/uk/co/wehavecookies56/kk/client/gui/GuiLockOn.java
+++ src/main/java/uk/co/wehavecookies56/kk/client/gui/GuiLockOn.java
@@ -1,17 +1,24 @@
 package uk.co.wehavecookies56.kk.client.gui;
 
+import java.awt.Color;
+
+import javax.swing.colorchooser.ColorSelectionModel;
+
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import uk.co.wehavecookies56.kk.client.core.handler.InputHandler;
 import uk.co.wehavecookies56.kk.common.capability.ModCapabilities;
 import uk.co.wehavecookies56.kk.common.capability.PlayerStatsCapability;
+import uk.co.wehavecookies56.kk.common.core.handler.ConfigHandler;
 import uk.co.wehavecookies56.kk.common.lib.Constants;
 import uk.co.wehavecookies56.kk.common.lib.Reference;
 
@@ -19,6 +26,11 @@ public class GuiLockOn extends GuiScreen {
 
 	int guiWidth = 256;
 	int guiHeight = 256;
+	
+	int hpGuiWidth = 173;
+	int hpBarWidth;
+	int hpGuiHeight = 10;
+	int noborderguiwidth = 171;
 
 	@SubscribeEvent
 	public void onRenderOverlayPost (RenderGameOverlayEvent event) {
@@ -30,7 +42,7 @@ public class GuiLockOn extends GuiScreen {
 			Minecraft mc = Minecraft.getMinecraft();
 			EntityPlayer player = mc.thePlayer;
 			float reduction = 4;
-			//mc.renderEngine.bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/lockon/lock on frame"+i+".png"));
+			mc.renderEngine.bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/lockon/lock on frame1.png"));
 
 			int screenWidth = event.getResolution().getScaledWidth();
 			int screenHeight = event.getResolution().getScaledHeight();
@@ -53,19 +65,140 @@ public class GuiLockOn extends GuiScreen {
 			// BG
 			if(target != null)
 			{
-				for(int i = 1; i<2;i++)
-				{
-					mc.renderEngine.bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/lockon/lock on frame"+i+".png"));
-					this.drawTexturedModalRect(0, 0, 0, 0, guiWidth, guiHeight);
-					
-				}
+				this.drawTexturedModalRect(0, 0, 0, 0, guiWidth, guiHeight);
 			}
 
 			GL11.glPopMatrix();
+			mc.renderEngine.bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/hpbar.png"));
+
 			GL11.glPushMatrix();
+
+			if(target != null)//TODO && player has scan ability
+			{
+				this.drawString(mc.fontRendererObj, target.getName(), screenWidth-mc.fontRendererObj.getStringWidth(target.getName()), 10, 0xFFFFFF);
+				drawHPBar(event, (EntityLivingBase)target);
+			}
+				
+			
 			GL11.glScalef(scale, scale, scale);
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 			GL11.glPopMatrix();
+		}
+	}
+	
+	public void drawHPBarBack (int posX, int posY, int width, float scale) {
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/hpbar.png"));
+		GL11.glPushMatrix();
+		{
+			GL11.glPushMatrix();
+			{
+				GL11.glTranslatef(scale * posX, scale * posY, 0);
+				GL11.glScalef(scale, scale, 0);
+				drawTexturedModalRect(0, 0, 0, 0, 2, 10);
+			}
+			GL11.glPopMatrix();
+
+			GL11.glPushMatrix();
+			{
+				GL11.glTranslatef((posX + 2) * scale, posY * scale, 0);
+				GL11.glScalef(width, scale, 0);
+				drawTexturedModalRect(0, 0, 2, 0, 1, 10);
+			}
+			GL11.glPopMatrix();
+
+			GL11.glPushMatrix();
+			{
+				GL11.glTranslatef((posX + 2) * scale + width, scale * posY, 0);
+				GL11.glScalef(scale, scale, 0);
+				drawTexturedModalRect(0, 0, 3, 0, 2, 10);
+			}
+			GL11.glPopMatrix();
+		}
+		GL11.glPopMatrix();
+
+	}
+	
+	public void drawHPBarTop (int posX, int posY, int width, float scale) {
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/hpbar.png"));
+		GL11.glPushMatrix();
+		{
+			GL11.glPushMatrix();
+			{
+				GL11.glTranslatef(scale * (posX + 2), scale * (posY + 2), 0);
+				GL11.glScalef(scale, scale, 0);
+				drawTexturedModalRect(0, 0, 2, 12, 2, 6);
+			}
+			GL11.glPopMatrix();
+
+			GL11.glPushMatrix();
+			{
+				GL11.glTranslatef((posX + 4) * scale, (posY + 2) * scale, 0);
+				GL11.glScalef(width, scale, 0);
+				drawTexturedModalRect(0, 0, 3, 12, 1, 6);
+			}
+			GL11.glPopMatrix();
+
+			GL11.glPushMatrix();
+			{
+				GL11.glTranslatef((posX + 4) * scale + width, scale * (posY + 2), 0);
+				GL11.glScalef(scale, scale, 0);
+				drawTexturedModalRect(0, 0, 4, 12, 2, 6);
+			}
+			GL11.glPopMatrix();
+			
+			GL11.glPushMatrix();
+			{
+				GL11.glTranslatef((posX + 17) * scale/2 + width, scale/2 * (posY), 0);
+				GL11.glScalef(scale, scale, 0);
+				drawTexturedModalRect(0, 0, 0, 32, 22, 12);
+			}
+			GL11.glPopMatrix();
+		}
+		GL11.glPopMatrix();
+	}
+	public void drawHPBar(RenderGameOverlayEvent event, EntityLivingBase target)
+	{
+		int screenWidth = event.getResolution().getScaledWidth();
+		int screenHeight = event.getResolution().getScaledHeight();
+		if(!Minecraft.getMinecraft().thePlayer.getCapability(ModCapabilities.PLAYER_STATS, null).getHudMode()) return;
+		if (event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
+
+			Minecraft mc = Minecraft.getMinecraft();
+			mc.renderEngine.bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/hpbar.png"));
+
+
+			float oneHeart = (noborderguiwidth / target.getMaxHealth());
+			int currHealth = noborderguiwidth - (int) (oneHeart * target.getHealth());
+			float scale = 0.65f;
+
+			hpBarWidth = (int) ((int) target.getHealth() * 1.5);
+
+			int hpBarMaxWidth = (int) ((int) target.getMaxHealth() * 1.5);
+
+			switch (mc.gameSettings.guiScale) {
+				case Constants.SCALE_AUTO:
+					scale = 0.85f;
+					break;
+				case Constants.SCALE_NORMAL:
+					scale = 0.85f;
+					break;
+				default:
+					scale = 0.70f;
+					break;
+			}
+
+			int posX = 190 - hpBarWidth;
+
+			GL11.glPushMatrix();
+			GL11.glTranslatef((screenWidth - hpBarMaxWidth * scale) - 10 * scale*2, 1, 0);
+			GL11.glScalef(scale, scale, scale);
+			drawHPBarBack(0, 0, hpBarMaxWidth, scale);
+			GL11.glPopMatrix();
+			GL11.glPushMatrix();
+			GL11.glTranslatef((screenWidth - (hpBarWidth) * scale) - 10 * scale*2, 1, 0);
+			GL11.glScalef(scale, scale, scale);
+			drawHPBarTop(0, 0, (int) ((hpBarWidth - (4 * scale))), scale);
+			GL11.glPopMatrix();		
 		}
 	}
 }
