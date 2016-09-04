93b3a605c7e0f17e7d78a950f85fb629dc51ec94
Abelatox <abel54225@gmail.com>
2016-09-04 11:21:39 +0200

    Forgot this one

---
+++ src/main/java/uk/co/wehavecookies56/kk/common/block/base/BlockKKOre.java
@@ -0,0 +1,32 @@
+package uk.co.wehavecookies56.kk.common.block.base;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.IBlockAccess;
+
+public class BlockKKOre extends Block {
+
+	public BlockKKOre (Material material, String toolClass, int level, float hardness, float resistance) {
+		super(material);
+		this.setHarvestLevel(toolClass, level);
+		setHardness(hardness);
+		setResistance(resistance);
+	}
+	private Random rand = new Random();
+	
+	@Override
+	public Item getItemDropped (IBlockState state, Random r, int fortune) {
+		return null;
+	}
+	
+	@Override
+	public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune) {
+			return MathHelper.getRandomIntegerInRange(rand, 2, 4);
+	}
+}
