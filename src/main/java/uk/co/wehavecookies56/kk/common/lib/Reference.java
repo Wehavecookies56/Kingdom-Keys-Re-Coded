ec937d3ca840d9ece66da4e150364b2cd9431bf1
Wehavecookies56 <qwenit56@gmail.com>
2016-09-16 23:10:51 +0100

    0.8.2.1

--- src/main/java/uk/co/wehavecookies56/kk/common/lib/Reference.java
+++ src/main/java/uk/co/wehavecookies56/kk/common/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MODID = "kk";
 	public static final String MODNAME = "Kingdom Keys Re:Coded";
-	public static final String MODVER = "0.8.2";
+	public static final String MODVER = "0.8.2.1";
 	public static final String MCVER = "1.10.2";
 	public static final String MODCHANNEL = MODID;
 	public static final String CLIENTPROXY = "uk.co.wehavecookies56.kk.client.core.proxy.ClientProxy";
