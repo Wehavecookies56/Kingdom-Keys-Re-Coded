28c511c602af548b759d9f5c3337467142db6e78
Nathan Stelken <NStelken312@gmail.com>
2017-09-11 11:25:11 -0500

    removed protals from lexicons

--- src/main/java/uk/co/wehavecookies56/kk/common/item/org/ItemLexicon.java
+++ src/main/java/uk/co/wehavecookies56/kk/common/item/org/ItemLexicon.java
@@ -37,39 +37,7 @@ public class ItemLexicon extends ItemOrgWeapon implements IOrgWeapon{
         return EnumRarity.UNCOMMON;
     }
 
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-        //Minecraft.getMinecraft().displayGuiScreen(new GuiOrgUnlock());
-        if(world.isRemote){
-            if (player.getCapability(ModCapabilities.ORGANIZATION_XIII, null).getMember() != Utils.OrgMember.NONE) {
-                if(!player.getCapability(ModCapabilities.PLAYER_STATS, null).getRecharge()){
-                    IOrganizationXIII orgXIII = player.getCapability(ModCapabilities.ORGANIZATION_XIII, null);
-
-                    if(orgXIII.getPortalX()!=0 && orgXIII.getPortalY()!=0 && orgXIII.getPortalZ()!=0){
-                        RayTraceResult rtr = InputHandler.getMouseOverExtended(100);
-                        if (rtr != null) {
-                            if (rtr.typeOfHit == rtr.typeOfHit.BLOCK){
-                                double distanceSq = player.getDistanceSq(rtr.getBlockPos());
-                                double reachSq = 100 * 100;
-                                if (reachSq >= distanceSq) {
-                                    BlockPos pos = rtr.getBlockPos();
-                                    BlockPos destination = new BlockPos(orgXIII.getPortalX(),orgXIII.getPortalY(),orgXIII.getPortalZ());
-                                    EntityOrgPortal portal = new EntityOrgPortal(player.world, player, pos, destination, orgXIII.getPortalDimension());
-                                    world.spawnEntity(portal);
-                                    PacketDispatcher.sendToServer(new OrgPortal(rtr.getBlockPos(),destination, orgXIII.getPortalDimension()));
-                                    player.world.playSound((EntityPlayer) player, player.getPosition(), ModSounds.lockon, SoundCategory.MASTER, 1.0f, 1.0f);
-                                }
-                            }
-                        }
-                    }else{
-                        player.sendMessage(new TextComponentString(TextFormatting.RED + "You don't have any portal destination"));
-                    }
-                }
-            }
-        }
-        return super.onItemRightClick(world, player, hand);
-    }
-
+    
     @Override
     public OrgMember getMember() {
         return Utils.OrgMember.ZEXION;
