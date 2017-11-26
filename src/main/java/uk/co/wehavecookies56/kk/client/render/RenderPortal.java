3c463b32d983dad17fa0dcbe4323b4cbeb7ac3ce
Abelatox <abel54225@gmail.com>
2017-11-26 12:01:49 +0100

    Added {} to Push and Pull
    Nothing really important

--- src/main/java/uk/co/wehavecookies56/kk/client/render/RenderPortal.java
+++ src/main/java/uk/co/wehavecookies56/kk/client/render/RenderPortal.java
@@ -34,23 +34,23 @@ public class RenderPortal  extends Render implements IRenderFactory<EntityOrgPor
     @Override
     public void doRender (Entity entity, double x, double y, double z, float yaw, float pitch) {
         GL11.glPushMatrix();
-        GL11.glTranslated(x, y+1, z);
-        
-        float ticks = entity.ticksExisted;
-        if(ticks < 10) //Growing
-        	GL11.glScalef(ticks*0.2f, ticks*0.2f, ticks*0.2f);
-        else if(ticks > 90) //Disappearing
-        	GL11.glScalef((100-ticks)*0.2f, (100-ticks)*0.2f, (100-ticks)*0.2f);
-        else //Static size
-        	GL11.glScalef(2.0f, 2.0f, 2.0f);
-        
-//        System.out.println(ticks);
-        GL11.glRotatef(-Minecraft.getMinecraft().player.getPitchYaw().y, 0, 1, 0);
-
-        bindEntityTexture(entity);
-
-        this.model.render(entity, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
-
+        {
+	        GL11.glTranslated(x, y+1, z);
+	        
+	        float ticks = entity.ticksExisted;
+	        if(ticks < 10) //Growing
+	        	GL11.glScalef(ticks*0.2f, ticks*0.2f, ticks*0.2f);
+	        else if(ticks > 90) //Disappearing
+	        	GL11.glScalef((100-ticks)*0.2f, (100-ticks)*0.2f, (100-ticks)*0.2f);
+	        else //Static size
+	        	GL11.glScalef(2.0f, 2.0f, 2.0f);
+	        
+	        GL11.glRotatef(-Minecraft.getMinecraft().player.getPitchYaw().y, 0, 1, 0);
+	
+	        bindEntityTexture(entity);
+	
+	        this.model.render(entity, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+        }
         GL11.glPopMatrix();
     }
 
