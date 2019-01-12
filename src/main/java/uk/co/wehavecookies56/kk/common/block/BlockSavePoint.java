1a746077017572d59ece73d3d18eb889a426822b
Abelatox <abel54225@gmail.com>
2019-01-12 16:19:53 +0100

    Save point particles effect & fixed chat spam for saving spawn
    Needs work on healing particles

--- src/main/java/uk/co/wehavecookies56/kk/common/block/BlockSavePoint.java
+++ src/main/java/uk/co/wehavecookies56/kk/common/block/BlockSavePoint.java
@@ -12,6 +12,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -31,126 +33,155 @@ import uk.co.wehavecookies56.kk.common.network.packet.client.SyncMagicData;
 
 public class BlockSavePoint extends Block {
 
-    long ticks = 0;
+	protected BlockSavePoint(Material material, String toolClass, int level, float hardness, float resistance, String name) {
+		super(material);
+		setHarvestLevel(toolClass, level);
+		setHardness(hardness);
+		setResistance(resistance);
+		setCreativeTab(ModBlocks.tabKingdomKeysBlocks);
+		setRegistryName(name);
+		setUnlocalizedName(name);
+		// setBlockBounds(0, 0, 0, 1, 0.1F, 1);
+		setTickRandomly(true);
+		// setSoundType(SoundType.STONE);
+	}
 
-    protected BlockSavePoint (Material material, String toolClass, int level, float hardness, float resistance, String name) {
-        super(material);
-        setHarvestLevel(toolClass, level);
-        setHardness(hardness);
-        setResistance(resistance);
-        setCreativeTab(ModBlocks.tabKingdomKeysBlocks);
-        setRegistryName(name);
-        setUnlocalizedName(name);
-        //setBlockBounds(0, 0, 0, 1, 0.1F, 1);
-        setTickRandomly(true);
-        //setSoundType(SoundType.STONE);
-    }
+	double a = 0;
+	long ticks = 0;
 
-    @Override
-    public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
-        world.scheduleUpdate(pos, this, tickRate(world));
-        super.onBlockAdded(world, pos, state);
-    }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void randomDisplayTick(IBlockState stateIn, World world, BlockPos pos, Random rand) {
+		/*
+		 * double r = 0.6D; double cx = pos.getX() + 0.5; double cy = pos.getY(); double
+		 * cz = pos.getZ() + 0.5;
+		 * 
+		 * // for (int a = 1; a <= 360; a += 7) { double x = cx + (r *
+		 * Math.cos(Math.toRadians(a))); double z = cz + (r *
+		 * Math.sin(Math.toRadians(a)));
+		 * 
+		 * double x2 = cx + (r * Math.cos(Math.toRadians(-a))); double z2 = cz + (r *
+		 * Math.sin(Math.toRadians(-a)));
+		 * 
+		 * System.out.println(a/180/2);
+		 * world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, x2, cy + 0.8D - a/360D,
+		 * z2, 0.0D, 0.0D, 0.0D); world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY,
+		 * x, cy + 0.2D + a/360D, z, 0.0D, 0.0D, 0.0D);
+		 * 
+		 * a++; if(a > 360) a = 0;
+		 */
+	}
 
-    @Override
-    public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entityIn) {
-        if (!world.isRemote) 
-        	updateState(world, pos);
-        //this.timeHealed = (int) Minecraft.getSystemTime() / 1000;
-    }
+	@Override
+	public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
+		world.scheduleUpdate(pos, this, tickRate(world));
+		super.onBlockAdded(world, pos, state);
+	}
 
-    @Override
-    public void updateTick (World world, BlockPos pos, IBlockState state, Random rand) {
-        if (!world.isRemote) 
-        	updateState(world, pos);
-    }
+	@Override
+	public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entityIn) {
+		if (!world.isRemote)
+			updateState(world, pos);
+	}
 
-    private void updateState (World world, BlockPos pos) {
-    	ticks++;
-        List list = world.getEntitiesWithinAABBExcludingEntity((Entity) null, new AxisAlignedBB(pos.add(0, 0, 0), pos.add(1, 1, 1)));
-        if (!list.isEmpty())
-        	for (int i = 0; i < list.size(); i++) {
-            Entity e = (Entity) list.get(i);
-            if (e instanceof EntityPlayer) {
-                EntityPlayer player = (EntityPlayer) e;
-                IPlayerStats STATS = player.getCapability(ModCapabilities.PLAYER_STATS, null);
+	@Override
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+		if (!world.isRemote)
+			updateState(world, pos);
+	}
 
-               // System.out.println(player.getBedLocation());
-               // System.out.println(pos.getX());
-                boolean samePos;
-                BlockPos bedPos;
-                if(player.getBedLocation() == null) {
-                	IFirstTimeJoin originalPos = player.getCapability(ModCapabilities.FIRST_TIME_JOIN, null);
-                	bedPos = new BlockPos(originalPos.getPosX(),originalPos.getPosY(),originalPos.getPosZ());
-                }else{
-                	bedPos = player.getBedLocation();
-                }
-                
-                samePos = bedPos.getX() == pos.getX() && bedPos.getY() == pos.getY() && bedPos.getZ() == pos.getZ();
+	private void updateState(World world, BlockPos pos) {
+		ticks++;
+		if (a > 360)
+			a = 0;
+		a += 10;
+		PacketDispatcher.sendToAll(new SpawnCureParticles(pos, true, (int) a));
+		List list = world.getEntitiesWithinAABBExcludingEntity((Entity) null, new AxisAlignedBB(pos.add(0, 0, 0), pos.add(1, 1, 1)));
+		if (!list.isEmpty())
+			for (int i = 0; i < list.size(); i++) {
+				Entity e = (Entity) list.get(i);
+				if (e instanceof EntityPlayer) {
+					EntityPlayer player = (EntityPlayer) e;
+					IPlayerStats STATS = player.getCapability(ModCapabilities.PLAYER_STATS, null);
 
-                if (player.isSneaking() && !samePos) {
-                    player.setSpawnChunk(pos, true, 0);
-                    player.setSpawnPoint(pos, true);
-                    TextHelper.sendFormattedChatMessage("Spawn point saved!", TextFormatting.GREEN, player);
-                    world.playSound((EntityPlayer)null, player.getPosition(), ModSounds.savespawn, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                }
-              
-                if(player.getHealth() < player.getMaxHealth() || STATS.getMP() < STATS.getMaxMP()){
-                    player.heal(1);
-                    STATS.addMP(2);
-                    if (player.getFoodStats().getFoodLevel() < 20)
-                    	player.getFoodStats().addStats(4, 0);
-                                  
-                    if (ticks % 20 == 0){
-                    	world.playSound((EntityPlayer)null, player.getPosition(), ModSounds.savepoint, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                    	PacketDispatcher.sendToAllAround(new SpawnCureParticles(pos, true), player, 64.0D);
-                    }
-                    PacketDispatcher.sendTo(new SyncMagicData(player.getCapability(ModCapabilities.MAGIC_STATE, null),STATS), (EntityPlayerMP)player);
-                }
-            }
-        }
-        world.scheduleUpdate(new BlockPos(pos), this, this.tickRate(world));
-    }
+					// System.out.println(player.getBedLocation());
+					// System.out.println(pos.getX());
+					boolean samePos;
+					BlockPos bedPos;
+					if (ticks % 10 == 0) {
+						if (player.getBedLocation() == null) {
+							IFirstTimeJoin originalPos = player.getCapability(ModCapabilities.FIRST_TIME_JOIN, null);
+							bedPos = new BlockPos(originalPos.getPosX(), originalPos.getPosY(), originalPos.getPosZ());
+						} else {
+							bedPos = player.getBedLocation();
+						}
 
-    @Override
-    public int tickRate (World worldIn) {
-        return 15;
-    }
+						samePos = bedPos.getX() == pos.getX() && bedPos.getY() == pos.getY() && bedPos.getZ() == pos.getZ();
 
-    @Override
-    @SideOnly (Side.CLIENT)
-    public BlockRenderLayer getBlockLayer () {
-        return BlockRenderLayer.TRANSLUCENT;
-    }
+						if (player.isSneaking() && !samePos) {
+							player.bedLocation = pos;
+							player.setSpawnChunk(pos, true, 0);
+							player.setSpawnPoint(pos, true);
+							TextHelper.sendFormattedChatMessage("Spawn point saved!", TextFormatting.GREEN, player);
+							world.playSound((EntityPlayer) null, player.getPosition(), ModSounds.savespawn, SoundCategory.BLOCKS, 1.0f, 1.0f);
+						}
+					}
+					
+					if (player.getHealth() < player.getMaxHealth() || STATS.getMP() < STATS.getMaxMP()) {
+						player.heal(1);
+						STATS.addMP(2);
+						if (player.getFoodStats().getFoodLevel() < 20)
+							player.getFoodStats().addStats(4, 0);
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World world, BlockPos pos) {
-        return new AxisAlignedBB(new BlockPos(0, 0, 0), new BlockPos(1, 0.1, 1));
-    }
+						if (ticks % 20 == 0) {
+							world.playSound((EntityPlayer) null, player.getPosition(), ModSounds.savepoint, SoundCategory.BLOCKS, 1.0f, 1.0f);
+							PacketDispatcher.sendToAllAround(new SpawnCureParticles(pos, true, 0), player, 64.0D);
+						}
+						PacketDispatcher.sendTo(new SyncMagicData(player.getCapability(ModCapabilities.MAGIC_STATE, null), STATS), (EntityPlayerMP) player);
+					}
+				}
+			}
+		world.scheduleUpdate(new BlockPos(pos), this, this.tickRate(world));
+	}
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess world, BlockPos pos) {
-        return new AxisAlignedBB(new BlockPos(0, 0, 0), new BlockPos(1, 0.1, 1));
-    }
+	@Override
+	public int tickRate(World worldIn) {
+		return 1;
+	}
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
+	}
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
+	@SuppressWarnings("deprecation")
+	@Override
+	public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World world, BlockPos pos) {
+		return new AxisAlignedBB(new BlockPos(0, 0, 0), new BlockPos(1, 0.1, 1));
+	}
 
+	@SuppressWarnings("deprecation")
+	@Override
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess world, BlockPos pos) {
+		return new AxisAlignedBB(new BlockPos(0, 0, 0), new BlockPos(1, 0.1, 1));
+	}
 
-    @Override
-    public Item getItemDropped (IBlockState state, Random r, int fortune) {
-        return Item.getItemFromBlock(this);
-    }
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random r, int fortune) {
+		return Item.getItemFromBlock(this);
+	}
 
 }
--- src/main/java/uk/co/wehavecookies56/kk/common/network/packet/client/SpawnCureParticles.java
+++ src/main/java/uk/co/wehavecookies56/kk/common/network/packet/client/SpawnCureParticles.java
@@ -12,74 +12,96 @@ import uk.co.wehavecookies56.kk.common.network.packet.AbstractMessage;
 
 public class SpawnCureParticles extends AbstractMessage.AbstractClientMessage<SpawnCureParticles> {
 
-    double x, y, z;
-    boolean savePoint;
-    int lvl;
+	double x, y, z;
+	boolean savePoint;
+	int lvl;
+	int a;
 
-    public SpawnCureParticles () {}
+	public SpawnCureParticles() {
+	}
 
-    public SpawnCureParticles (Entity entity, int level) {
-        x = entity.posX;
-        y = entity.posY+entity.getEyeHeight();
-        z = entity.posZ;
-        lvl = level;
-    }
+	public SpawnCureParticles(Entity entity, int level) {
+		x = entity.posX;
+		y = entity.posY + entity.getEyeHeight();
+		z = entity.posZ;
+		lvl = level;
+		a = 0;
+	}
 
-    public SpawnCureParticles (BlockPos pos, boolean savepoint) {
-        x = pos.getX();
-        y = pos.getY();
-        z = pos.getZ();
-        savePoint = savepoint;
+	public SpawnCureParticles(BlockPos pos, boolean savepoint, int a) {
+		x = pos.getX();
+		y = pos.getY();
+		z = pos.getZ();
+		savePoint = savepoint;
+		this.a = a;
+	}
 
-    }
+	@Override
+	protected void read(PacketBuffer buffer) throws IOException {
+		x = buffer.readDouble();
+		y = buffer.readDouble();
+		z = buffer.readDouble();
+		savePoint = buffer.readBoolean();
+		lvl = buffer.readInt();
+		a = buffer.readInt();
+	}
 
-    @Override
-    protected void read (PacketBuffer buffer) throws IOException {
-        x = buffer.readDouble();
-        y = buffer.readDouble();
-        z = buffer.readDouble();
-        savePoint = buffer.readBoolean();
-        lvl = buffer.readInt();
-    }
+	@Override
+	protected void write(PacketBuffer buffer) throws IOException {
+		buffer.writeDouble(x);
+		buffer.writeDouble(y);
+		buffer.writeDouble(z);
+		buffer.writeBoolean(savePoint);
+		buffer.writeInt(lvl);
+		buffer.writeInt(a);
+	}
 
-    @Override
-    protected void write (PacketBuffer buffer) throws IOException {
-        buffer.writeDouble(x);
-        buffer.writeDouble(y);
-        buffer.writeDouble(z);
-        buffer.writeBoolean(savePoint);
-        buffer.writeInt(lvl);
-    }
+	@Override
+	public void process(EntityPlayer player, Side side) {
+		double r;
+		switch (this.lvl) {
+		case 1:
+			player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.5, this.z, 0.0D, 1.0D, 0.0D);
+			player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.8, this.z, 0.0D, 1.0D, 0.0D);
+			player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.2, this.z, 0.0D, 1.0D, 0.0D);
+			player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x + 0.3, this.y + 1.5, this.z, 0.0D, 1.0D, 0.0D);
+			player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x - 0.3, this.y + 1.5, this.z, 0.0D, 1.0D, 0.0D);
+			player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.5, this.z + 0.3, 0.0D, 1.0D, 0.0D);
+			player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.5, this.z - 0.3, 0.0D, 1.0D, 0.0D);
+			break;
+		}
 
-    @Override
-    public void process (EntityPlayer player, Side side) {
-        double r;
-        switch (this.lvl) {
-            case 1:
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.5, this.z, 0.0D, 1.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.8, this.z, 0.0D, 1.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.2, this.z, 0.0D, 1.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x + 0.3, this.y + 1.5, this.z, 0.0D, 1.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x - 0.3, this.y + 1.5, this.z, 0.0D, 1.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.5, this.z+0.3, 0.0D, 1.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, this.x, this.y + 1.5, this.z-0.3, 0.0D, 1.0D, 0.0D);
-                break;
-        }
+		if (savePoint) {
+			r = 0.8D;
+			double cx = this.x + 0.5;
+			double cy = this.y;
+			double cz = this.z + 0.5;
 
-        if (savePoint) {
-            r = 0.6D;
-            x = x + 0.6;
-            z = z + 0.6;
-            for (int a = 1; a <= 360; a += 7) {
-                double x = this.x-0.1 + (r * Math.cos(Math.toRadians(a)));
-                double z = this.z-0.1 + (r * Math.sin(Math.toRadians(a)));
+			// for (int a = 1; a <= 360; a += 7) {
+			double x = cx + (r * Math.cos(Math.toRadians(a)));
+			double z = cz + (r * Math.sin(Math.toRadians(a)));
 
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, x, this.y + 1.5D, z, 0.0D, 0.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, x, this.y + 1.05D, z, 0.0D, 0.0D, 0.0D);
-                player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, x, this.y + 0.5D, z, 0.0D, 0.0D, 0.0D);
+			double x2 = cx + (r * Math.cos(Math.toRadians(-a)));
+			double z2 = cz + (r * Math.sin(Math.toRadians(-a)));
 
-            }
-        }
-    }
+			player.world.spawnParticle(EnumParticleTypes.REDSTONE, x2, cy + 0.6D - Math.abs(a - 180) / 360D, z2, 0.1D, 1D, 0.1D);
+			player.world.spawnParticle(EnumParticleTypes.REDSTONE, x, cy + 0.1D + Math.abs(a - 180) / 360D, z, 0.1D, 1D, 0.1D);
+
+			/*
+			 * r = 0.6D; x = x + 0.6; z = z + 0.6; for (int a = 1; a <= 360; a += 7) {
+			 * double x = this.x-0.1 + (r * Math.cos(Math.toRadians(a))); double z =
+			 * this.z-0.1 + (r * Math.sin(Math.toRadians(a)));
+			 * 
+			 * player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, x, this.y +
+			 * 1.5D, z, 0.0D, 0.0D, 0.0D);
+			 * player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, x, this.y +
+			 * 1.05D, z, 0.0D, 0.0D, 0.0D);
+			 * player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, x, this.y +
+			 * 0.5D, z, 0.0D, 0.0D, 0.0D);
+			 * 
+			 * }
+			 */
+		}
+	}
 
 }
